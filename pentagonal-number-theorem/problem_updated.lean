dca31a49d37ba23b7cd9e5aed9f0feae4ad86907
Kenny Lau <kc_kennylau@yahoo.com.hk>
2026-07-13 21:45:15 +0100

    Updated versions of the artifacts

---
+++ pentagonal-number-theorem/problem_updated.lean
@@ -0,0 +1,15 @@
+import Mathlib
+import RogersRamanujan
+set_option backward.isDefEq.respectTransparency false
+
+open PowerSeries.WithPiTopology
+open scoped DiscreteUniformity
+
+def generalizedPentagonal (k : ℤ) : ℕ := (k * (3 * k - 1) / 2).toNat
+
+theorem pentagonal_number_theorem' :
+    HasSum
+      (fun k : ℤ =>
+        (-1 : PowerSeries ℤ) ^ k.natAbs * PowerSeries.X ^ generalizedPentagonal k)
+      (qPochhammerInf (PowerSeries.X : PowerSeries ℤ) PowerSeries.X) := by
+  sorry
